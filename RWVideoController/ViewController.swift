2cf2233d5d8107c14b0997c21e5e7871093ebcce
Raditya Kurnianto <raditya@detik.com>
2019-07-02 11:18:21 +0700

    Initial Commit

---
+++ RWVideoController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RWVideoController
+//
+//  Created by Raditya Kurnianto on 7/2/19.
+//  Copyright © 2019 raditya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
